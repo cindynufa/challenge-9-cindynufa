68076527927016e3ca4423959013921387741a70
Cindy Nufa <cindynufa@gmail.com>
2025-10-03 23:08:41 +0700

    Feat: added logo sc at ui component

---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,22 @@
+'use-client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Logo() {
+  return (
+    <Link
+      href="/"
+      className="flex flex-row gap-9.19 items-center cursor-pointer"
+    >
+      <Image
+        src="/assets/icons/logo.png"
+        alt="Logo"
+        className="w-42 h-42"
+        width={20}
+        height={20}
+      />
+      <h1 className="text-2xl font-bold text-neutral-950">Shirt</h1>
+    </Link>
+  );
+}
