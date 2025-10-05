d05d60ad608191e13a3315a765a8ddd46ae0fca6
Cindy Nufa <cindynufa@gmail.com>
2025-10-05 15:53:19 +0700

    Feat: added link to store page

---
+++ src/components/ui/store-profile.tsx
@@ -0,0 +1,23 @@
+"use-client";
+
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
+import { Link } from 'lucide-react';
+
+export default function storeProfile() {
+  return (
+    <Link href="/buyer/storePage">
+      <div className="flex flex-row gap-4">
+        <Avatar>
+          <AvatarImage src="/assets/images/img-store-1.png" />
+          <AvatarFallback>Photo Profile Store</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col">
+          <h3 className="text-base font-bold text-neutral-950">
+            Toko Barokah Jaya
+          </h3>
+          <p className="text-base text-neutral-950">Jakarta Selatan</p>
+        </div>
+      </div>
+    </Link>
+  );
+}
