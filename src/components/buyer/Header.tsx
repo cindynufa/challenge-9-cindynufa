53c820580aa962219b91adc47258788495e75636
Cindy Nufa <cindynufa@gmail.com>
2025-10-06 11:50:03 +0700

    Feat: added link to catalog page

--- src/components/buyer/Header.tsx
+++ src/components/buyer/Header.tsx
@@ -13,16 +13,19 @@ export default function Header() {
 
         {/* Catalog and Search */}
         <div className="flex flex-row gap-3 cursor-pointer">
-          <div className="flex flex-row h-11 px-4 gap-1.5 items-center border rounded-xl border-neutral-300">
-            <Image
-              src="/assets/icons/icon-catalog.png"
-              alt="Icon Catalog"
-              className="w-5 h-5"
-              width={20}
-              height={20}
-            />
-            <p className="text-sm text-neutral-950">Catalog</p>
-          </div>
+          <Link href="/buyer/catalogPage">
+            <div className="flex flex-row h-11 px-4 gap-1.5 items-center border rounded-xl border-neutral-300">
+              <Image
+                src="/assets/icons/icon-catalog.png"
+                alt="Icon Catalog"
+                className="w-5 h-5"
+                width={20}
+                height={20}
+              />
+              <p className="text-sm text-neutral-950">Catalog</p>
+            </div>
+          </Link>
+
           <div className="flex flex-row w-491 h-11 py-2 px-4 gap-1.5 items-center border rounded-xl border-neutral-300">
             <Image
               src="/assets/icons/icon-Search.png"
