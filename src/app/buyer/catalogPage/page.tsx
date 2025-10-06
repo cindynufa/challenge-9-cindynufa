65daf1966111b80301ad19443897bbe8243ebdf1
Cindy Nufa <cindynufa@gmail.com>
2025-10-06 11:50:35 +0700

    Feat: first commit for catalog page

---
+++ src/app/buyer/catalogPage/page.tsx
@@ -0,0 +1,21 @@
+'use-client';
+
+import Header from '@/components/buyer/Header';
+import Footer from '@/components/buyer/Footer';
+
+export default function CatalogPage() {
+  return (
+    <>
+      {/* Header */}
+      <Header />
+
+      {/* Catalog Page */}
+      <section>
+        <div></div>
+      </section>
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
