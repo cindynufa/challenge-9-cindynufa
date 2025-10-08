b7acc3dd4d1db2601b63db8be5c39f8a767a4eb4
Cindy Nufa <cindynufa@gmail.com>
2025-10-08 12:48:24 +0700

    Feat: added cart page, checkout page, and open store page for buyer role

---
+++ src/app/buyer/cartPage/page.tsx
@@ -0,0 +1,19 @@
+import Footer from '@/components/buyer/Footer';
+import Header from '@/components/buyer/Header';
+
+export default function CartPage() {
+  return (
+    <>
+      {/* Header */}
+      <Header />
+
+      {/* Content */}
+      <section>
+        <div></div>
+      </section>
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
