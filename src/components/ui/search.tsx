f8864c8b529772d0867e08b6cc64d4117623cd5c
Cindy Nufa <cindynufa@gmail.com>
2025-10-10 15:23:02 +0700

    Feat: added search ui component

--- src/components/ui/search.tsx
+++ src/components/ui/search.tsx
@@ -7,7 +7,7 @@ export default function Search() {
   return (
     <div className="flex flex-row w-full h-11 py-2 px-4 gap-1.5 items-center border rounded-xl border-neutral-300">
       <Image
-        src="/assets/icons/icon-Search.png"
+        src="/assets/icons/icon-search.png"
         alt="Icon Search"
         className="w-5 h-5"
         width={20}
